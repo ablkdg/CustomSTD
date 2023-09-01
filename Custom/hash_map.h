a9c858dee7cd62aa945d1eba6d539aafc6bf4df6
ablkdg <62139666+ablkdg@users.noreply.github.com>
2023-09-01 20:30:42 +0300

    hash_map basic methods

--- Custom/hash_map.h
+++ Custom/hash_map.h
@@ -4,11 +4,63 @@
 
 #ifndef CUSTOM_HASHMAP_H
 #define CUSTOM_HASHMAP_H
-namespace{
-    template<typename Type>
+
+#include <cstdint>
+#include <memory>
+
+namespace custom{
+    template<typename KeyType, typename DataType, int depth = 10>
     class hash_map{
+    public:
+        void insert(KeyType first, DataType second);
 
+    public:
+        hash_map(){
+            for (auto& element:m_bucket) {
+                element = new Node();
+            }
+        }
+        struct Node{
+            uint32_t key;
+            KeyType first;
+            DataType second;
+            Node* next;
+
+            Node(uint32_t k, KeyType f, DataType s, Node* n = nullptr) : key(k), first(f), second(s), next(n) {}
+            Node() : key(0), next(nullptr){}
+        };
+
+        ~hash_map();
+    private:
+        Node *m_bucket[depth];
+        std::hash<KeyType> m_Hasher;
     };
-
 }
+
+template<typename KeyType, typename DataType, int depth>
+void custom::hash_map<KeyType, DataType, depth>::insert(KeyType first, DataType second) {
+    uint32_t key = m_Hasher(first);
+    int bucketIndex = key % depth;
+
+    if(m_bucket[bucketIndex]->key == 0){
+        m_bucket[bucketIndex] = new Node(key, first, second);
+    } else {
+        Node* holder = m_bucket[bucketIndex];
+        m_bucket[bucketIndex]  = new Node(key, first, second, holder);
+    }
+}
+
+template<typename KeyType, typename DataType, int depth>
+custom::hash_map<KeyType, DataType, depth>::~hash_map() {
+    {
+        for (Node *node: m_bucket) {
+            while (node) {
+                Node *temp = node;
+                node = node->next;
+                delete temp;
+            }
+        }
+    }
+}
+
 #endif //CUSTOM_HASHMAP_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-
+#include "Custom/hash_map.h"
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    custom::hash_map<std::string, int, 1> test{};
+    test.insert("as",2);
+    test.insert("df",3);
+
     return 0;
 }
